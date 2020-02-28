26773e1d376d0aff750cd452a0bf9683da4ace68
norio c <norionorioc@gmail.com>
2020-02-28 02:54:46 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,8 +1,9 @@
 # require libraries/modules here
 
-html = File.read('fixtures/kickstarter.html')
-kickstarter = Nokogiri::HTML(html)
 
 def create_project_hash
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+
   # write your code here
 end
