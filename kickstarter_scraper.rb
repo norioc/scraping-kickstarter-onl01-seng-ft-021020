795b67345572e839821c9830230f59ce672ad9da
norio c <norionorioc@gmail.com>
2020-02-28 03:11:30 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,15 +3,22 @@ require "pry"
 
 
 def create_project_hash
-  html = File.read('fixtures/kickstarter.html')
-  kickstarter = Nokogiri::HTML(html)
+    html = File.read('fixtures/kickstarter.html')
+    kickstarter = Nokogiri::HTML(html)
+  
+  projects = {}
+  
+    kickstarter.css("li.project.grid_4").each do |project| projects[project]={}
+  
+  end
+  
+  projects
 
-  binding.pry 
 end
 
 create_project_hash
 
-# projects: kickstarter.css("li.project.grid_4")
+
 
 # title: project.css("h2.bbcard_name strong a").text
 
