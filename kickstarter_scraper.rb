e12f5fa39afa3e93950900118ef8b2106c3434a9
norio c <norionorioc@gmail.com>
2020-02-28 03:09:52 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -18,3 +18,5 @@ create_project_hash
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 
 # description: project.css("p.bbcard_blurb").text
+
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
